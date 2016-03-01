562c82a57854ec58b5190868e9c9b8ba59e01638
personabe1984 <personabe1984@gmail.com>
2016-02-29 23:33:28 -0500

    now user can put in anyword

--- src/nyc/c4q/personabe1984/Hangman.java
+++ src/nyc/c4q/personabe1984/Hangman.java
@@ -5,7 +5,11 @@ public class Hangman {
 
     public static void main(String[] args){
 
-        Game game = new Game("Treehouse");
+        if(args.length == 0){
+            System.out.println("Please enter a word");
+            System.exit(0);
+        }
+        Game game = new Game(args[0]);
 
         Prompter prompter = new Prompter(game);
         prompter.play();
